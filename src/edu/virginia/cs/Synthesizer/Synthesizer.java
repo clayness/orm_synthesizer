345e4ab7ea9de2c7c6c760a5df6a2da9fbb7ff7e
Clay Stevens <clay.stevens@huskers.unl.edu>
2020-04-15 07:26:48 -0500

    extracted args, methods, dropped unused

--- src/edu/virginia/cs/Synthesizer/Synthesizer.java
+++ src/edu/virginia/cs/Synthesizer/Synthesizer.java
@@ -1,97 +1,50 @@
 package edu.virginia.cs.Synthesizer;
 
-import edu.mit.csail.sdg.alloy4.Err;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.PrintStream;
+import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
-import java.util.*;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TimeZone;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.regex.Pattern;
 
-/**
- * Created by IntelliJ IDEA. User: ct4ew Date: 7/23/13 Time: 3:20 PM To change
- * this template use File | Settings | File Templates.
- */
+import edu.mit.csail.sdg.alloy4.Err;
+
 public class Synthesizer {
 
-	static String workspace = ".";// UI.workspace;
-	PrintStream file = null;
-	FileOutputStream output = null;
-	String trimmedFilename = "";
+	private static final Logger logger = Logger.getLogger(Synthesizer.class.getName());
 
-	// The list of quality equivalence classes on the Pareto optimal frontier.
-	ArrayList<MetricValue> solutionsMV = new ArrayList<MetricValue>();
-	// The list of Solutions on the Pareto optimal frontier.
-	ArrayList<MetricValue> paretoOptimalSolutions = new ArrayList<MetricValue>();
-	private Integer overallNIC = 0;
-	static boolean storeAllSolutions = false;
-
-	public static void main(String[] args) {
-		// suppose we already get the solutions of AlloyOM
-		// then we need to parse the solutions to get the data structure and the
-		// tAssociate
-		// after get the information of tAssociation, we can parse the AlloyOM
-		// to get the Alloy Instance Model
-		// the execute the Alloy Instance Model
-		// after each solution of AIM comes out, we parse it to different data
-		// schema, and then go to the next one
-
-		// Chong: we need to generate the solution first
-		workspace = args[0];
-		String alloyFile = args[1];
-		int maxSolNoParam = 1000000; // Integer.parseInt(args[2]);
-		if (args.length > 2) {
-			maxSolNoParam = Integer.parseInt(args[2]);
+	private static void delete(File f) {
+		if (f.isDirectory()) {
+			for (File c : f.listFiles()) {
+				delete(c);
+			}
 		}
-		if ((args.length > 3) && (args[3].equalsIgnoreCase("all"))) {
-			storeAllSolutions = true;
+		if (!f.delete()) {
+			logger.log(Level.SEVERE, "Failed to delete file: " + f.getAbsolutePath());
 		}
-		// if(args.length > 2)
-		// workspace = args[2];
-		// int maxSolNoParam = 1;
-		String solutionDirectory = workspace; // + "\\Solutions";
-		// String mergedFile = workspace + "\\"+ appType + "\\" + appDesc + "_"
-		// + archStyle + ".als";
+	}
 
-		// delete all files and folders in Solution Folder
-		File f = new File(solutionDirectory);
-		if (!f.exists()) {
-			f.mkdir();
-		}
-		solutionDirectory = f.getAbsolutePath();
-
-		File f1 = new File(args[1]);
-		String om = f1.getAbsolutePath();
-
-//		solutionDirectory = solutionDirectory
-//				+ File.separator
-//				+ om.substring(om.lastIndexOf(File.separator) + 1,
-//						om.lastIndexOf("."));
-		File f2 = new File(solutionDirectory);
-		delete(f2);
-		if (!f2.exists()) {
-			f2.mkdir();
-		}
+	private static void execute(File workspaceFolder, File alloyFile, int limit, int intScope, int range) {
+		// get mapping_run file first
+		String runFile = FileOperation.getMappingRun(alloyFile.getAbsolutePath());
 
 		try {
-			// get mapping_run file first
-			String runFile = FileOperation.getMappingRun(om);
-			new SmartBridge(solutionDirectory, runFile, maxSolNoParam);
+			new SmartBridge(workspaceFolder.getAbsolutePath(), runFile, limit);
 		} catch (Err err) {
-			err.printStackTrace();
+			logger.log(Level.SEVERE, "error solving for mapping file: "+runFile, err);
+			System.exit(2);
 		}
 
 		// (1) parse the AlloyOM solutions to get the data structure and the
 		// tAssociation
-		String folderOfAlloyOMSols = solutionDirectory;
-		File file = new File(folderOfAlloyOMSols);
-		if (!file.isAbsolute()) {
-			folderOfAlloyOMSols = file.getAbsolutePath();
-		}
-		// String folderOfAlloyOMSols = "C:\\Users\\ct4ew\\Desktop\\CSOS\\";
 		HashMap<String, HashMap<String, ArrayList<CodeNamePair<String>>>> schemas = new HashMap<>();
 		HashMap<String, ArrayList<CodeNamePair<String>>> parents = new HashMap<>();
 		HashMap<String, ArrayList<CodeNamePair<String>>> reverseTAss = new HashMap<>();
@@ -102,49 +55,21 @@ public class Synthesizer {
 		HashMap<String, ArrayList<CodeNamePair<String>>> fieldsTable = new HashMap<>();
 		HashMap<String, ArrayList<String>> allFields = new HashMap<>();
 		HashMap<String, ArrayList<CodeNamePair<String>>> fieldType = new HashMap<>();
-		boolean isRandom;
 
-		String alloyOM = args[1];
+		String alloyInstanceModel = alloyFile.getAbsolutePath().replace(".als", "_dm.als");
 
-		file = new File(alloyOM);
-		if (!file.isAbsolute()) {
-			alloyOM = file.getAbsolutePath();
-		}
-		// String alloyInstanceModel =
-		// "C:\\Users\\ct4ew\\Desktop\\CSOS\\decider_dm.als";
-		// String arg3 = args[2];
-		String pattern = Pattern.quote(System.getProperty("file.separator"));
-		String[] paths = alloyOM.split(pattern);
-		String OMName = paths[paths.length - 1];
-		OMName = OMName.substring(0, OMName.length() - 4) + "_dm.als";
-		String alloyInstanceModel = "";
-		for (int i = 0; i < paths.length - 1; i++) {
-			alloyInstanceModel += paths[i] + File.separator;
-		}
-		alloyInstanceModel += OMName;
-		// String alloyInstanceModel = args[2];
-		// String alloyInstanceModel =
-		// "C:\\Users\\ct4ew\\Desktop\\CSOS\\csos_dm.als";
-		int intScope = 6; // default
-		if (args.length >= 3) {
-			intScope = Integer.valueOf(args[2]);
-		}
 		AlloyOMToAlloyDM aotad = new AlloyOMToAlloyDM();
-		aotad.run(alloyOM, alloyInstanceModel, intScope);
+		aotad.run(alloyFile.getAbsolutePath(), alloyInstanceModel, intScope);
 		ArrayList<String> ids = aotad.getIDs();
 		ArrayList<String> associations = aotad.getAss();
 		HashMap<String, String> typeList = aotad.getTypeList();
 		ArrayList<Sig> sigs = aotad.getSigs();
 
-		File dir = new File(folderOfAlloyOMSols);
-		for (File singleFile : dir.listFiles()) {
+		for (File singleFile : workspaceFolder.listFiles()) {
 			// Do something with child
-			if (singleFile.getName().contains("xml")) {
+			if (singleFile.getName().endsWith(".xml")) {
 				String fileName = singleFile.getPath();
-				String schemaName = singleFile.getName();
-				schemaName = schemaName.substring(0, schemaName.length() - 4);
-				String dbSchemaFile = folderOfAlloyOMSols + File.separator
-						+ schemaName + ".sql";
+				String dbSchemaFile = singleFile.getAbsolutePath().replace(".xml", ".sql");
 				ORMParser parser = new ORMParser(fileName, dbSchemaFile, sigs);
 				parser.createSchemas();
 
@@ -162,97 +87,85 @@ public class Synthesizer {
 		}
 
 		// (2) parse the AlloyOM to get Alloy Instance Model
-		SolveAlloyDM solver = new SolveAlloyDM(schemas, parents, reverseTAss,
-				foreignKeys, association, primaryKeys, fields, allFields,
-				fieldsTable, fieldType, ids, associations, intScope, typeList,
-				sigs);
-		for (Map.Entry<String, HashMap<String, ArrayList<CodeNamePair<String>>>> entry : schemas
-				.entrySet()) {
+		SolveAlloyDM solver = new SolveAlloyDM(schemas, parents, reverseTAss, foreignKeys, association, primaryKeys,
+				fields, allFields, fieldsTable, fieldType, ids, associations, intScope, typeList, sigs);
+		for (Map.Entry<String, HashMap<String, ArrayList<CodeNamePair<String>>>> entry : schemas.entrySet()) {
 			String dbScheme = entry.getKey();
 			System.out.print("NCT of " + dbScheme + ": ");
 			System.out.println(solver.getNCTSum(dbScheme));
 			System.out.println("-------------------------------");
 		}
-		for (Map.Entry<String, HashMap<String, ArrayList<CodeNamePair<String>>>> entry : schemas
-				.entrySet()) {
+		for (Map.Entry<String, HashMap<String, ArrayList<CodeNamePair<String>>>> entry : schemas.entrySet()) {
 			String dbScheme = entry.getKey();
 			System.out.print("TATI of " + dbScheme + ": ");
 			System.out.println(solver.getTATISum(dbScheme));
 			System.out.println("-------------------------------");
 		}
 
-		
-			isRandom = false; //Boolean.valueOf(args[3]);
-			int range = 5000; //Integer.valueOf(args[4]);
-			/**
-			 * Chong: For qualifying exam, I will generate random test cases as
-			 * well as synthesized test cases
-			 */
-
-			// if (!isRandom) {
-//			try {
-			/**
-			 * Chong: synthesize test cases first, and then generate random test cases
-			 */
-//			solver.solveWithContinueUpdate(alloyInstanceModel, folderOfAlloyOMSols);
-			
-//		} catch (Err err) {
-		// err.printStackTrace(); // To change body of catch statement use File
-		// // | Settings | File Templates.
-		// } catch (FileNotFoundException e) {
-		// e.printStackTrace(); // To change body of catch statement use File |
-		// // Settings | File Templates.
-		// }
-			// solver.solveModel(alloyInstanceModel);
-			// } else {
-			
-			/**
-			 * generate random test cases starts here
-			 */
-			int newRange = 5000;
-			long start = System.currentTimeMillis();
-			solver.getOutPutOrders(alloyOM);
-			int i = 0; // for decider
-			// range = 164812;
-			SimpleDateFormat sdf;
-			Date date_elapsed;
-			while (i < range) {
-				int low = i;
-				int up = i + newRange;
-				if (up > range) {
-					up = range;
-				}
-				solver.randomInstanceGenerator(low + 1, up);
-				solver.generateInsert();
-				// solver.generateUpdate();
-				solver.generateSelect1();
-				solver.printAllStatements(1);
-				long now = System.currentTimeMillis();
-				sdf = new SimpleDateFormat("HH:mm:ss");
-				sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
-				long elapsed = now - start;
-				date_elapsed = new Date(elapsed);
-				System.out.println(up + " queries are generated within time: "
-						+ sdf.format(date_elapsed));
-				i = i + newRange;
-			}
-			// }
-
+		generateSql(alloyFile, range, solver);
 	}
 
-	public static void delete(File f) {
-		if (f.isDirectory()) {
-			for (File c : f.listFiles()) {
-				delete(c);
+	private static void generateSql(File alloyFile, int range, SolveAlloyDM solver) {
+		int newRange = 5000;
+		long start = System.currentTimeMillis();
+		solver.getOutPutOrders(alloyFile.getAbsolutePath());
+		SimpleDateFormat sdf;
+		Date date_elapsed;
+		for (int i = 0; i < range; i += newRange) {
+			int low = i;
+			int up = i + newRange;
+			if (up > range) {
+				up = range;
 			}
+			solver.randomInstanceGenerator(low + 1, up);
+			solver.generateInsert();
+			// solver.generateUpdate();
+			solver.generateSelect1();
+			solver.printAllStatements(1);
+			long now = System.currentTimeMillis();
+			sdf = new SimpleDateFormat("HH:mm:ss");
+			sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
+			long elapsed = now - start;
+			date_elapsed = new Date(elapsed);
+			System.out.println(up + " queries are generated within time: " + sdf.format(date_elapsed));
 		}
-		if (!f.delete()) {
-			try {
-				throw new FileNotFoundException("Failed to delete file: " + f);
-			} catch (FileNotFoundException ex) {
-				Logger.getLogger(Synthesizer.class.getName()).log(Level.SEVERE, null,
-						ex);
-			}
+	}
+
+	public static void main(String[] args) {
+		if (args.length < 2) {
+			logger.severe("invalid arguments: Synthesizer <workspace> <alloy model> [sol limit] [int scope] [range]");
+			System.exit(1);
 		}
+
+		String workspace = args[0];
+		String alloyPath = args[1];
+
+		int maxSolNoParam = 1000000;
+		if (args.length > 2) {
+			maxSolNoParam = Integer.parseInt(args[2]);
+		}
+
+		int intScope = 6;
+		if (args.length > 3) {
+			intScope = Integer.parseInt(args[3]);
+		}
+
+		int range = 5000;
+		if (args.length > 4) {
+			range = Integer.parseInt(args[4]);
+		}
+
+		String leaf = ZonedDateTime.now(ZoneOffset.UTC).format(DateTimeFormatter.ofPattern("uuuuMMdd'T'HHmmss'Z'"));
+		File workspaceFolder = Paths.get(workspace, leaf).toFile();
+		logger.info("writing output to path: " + workspaceFolder.getAbsolutePath());
+		if (workspaceFolder.exists()) {
+			logger.warning("output folder exists, clearing all contents: " + workspaceFolder.getAbsolutePath());
+			delete(workspaceFolder);
+		}
+		workspaceFolder.mkdirs();
+
+		File alloyFile = new File(alloyPath);
+		logger.info("reading Alloy model from file: " + alloyFile.getAbsolutePath());
+		execute(workspaceFolder, alloyFile, maxSolNoParam, intScope, range);
 	}
 }
